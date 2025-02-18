33a325bad7d4c301ec658a853da5322c838a92a1
pikeeee <powpow824@gmail.com>
2025-02-19 02:20:05 +0700

    init tailwind

--- lms-tutorial/tailwind.config.js
+++ lms-tutorial/tailwind.config.js
@@ -5,9 +5,9 @@ const { withUt } = require("uploadthing/tw");
 module.exports = withUt({
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
